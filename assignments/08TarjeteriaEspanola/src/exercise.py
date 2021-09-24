6911d8d46486eb158cdcd0b575e22e1b5ad70f22
BryantOchoa <A01252419@itesm.mx>
2021-09-24 17:17:42 +0000

    quiz 4 bryant

--- assignments/08TarjeteriaEspanola/src/exercise.py
+++ assignments/08TarjeteriaEspanola/src/exercise.py
@@ -1,7 +1,15 @@
-
+def tarjeteria(x,y):
+    pliego = x*12
+    plumon = y*35
+    if pliego<plumon:
+        return(pliego)
+    elif pliego>plumon:
+        return(plumon)
 def main():
-    #escribe tu código abajo de esta línea
-    
+    pliego = int(input('Dame la cantidad de pliegos de papel albanene: '))
+    plumon = int(input('Dame la cantidad de plumones: '))
+    total = tarjeteria(pliego,plumon)
+    print('El número máximo de tarjetas que se pueden hacer es: '+str(total))
 
 if __name__=='__main__':
     main()
